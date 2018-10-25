3e2e993d947142122e68f24b5c57c6f39be39f5d
serega <ser191097@gmail.com>
2018-10-26 00:00:42 +0300

    work

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 const int w = 8;
-const double eps = FLT_EPSILON/10;
+const double eps = FLT_EPSILON;
 
 
 
@@ -18,16 +18,14 @@ int sgn(T val) {
 
 
 
-void mulmatr(int n, double **a,double **t, double cosf, double sinf, int i, int j){
+void mulmatr(int n, double **a,double **t, double cosf, double sinf, int i, int j, double &end, int mode, double *sumstr){
 	double tempstri[n], tempstrj[n], tempclni[n], tempclnj[n];
 	for (int k = 0 ; k < n; ++k) {
 		tempstri[k] = cosf * a[i][k] - sinf * a[j][k];
 		tempstrj[k] = sinf * a[i][k] + a[j][k] * cosf;
 		//cout << "i" << k << ' ' << tempstri[k] << endl;
 		//cout << "j" << k << ' ' << tempstrj[k] << endl;
-		//использовать tempstri;
 	}
-	cout << endl << endl;
 	for (int k = 0 ; k < n; ++k) {
 		tempclni[k] = a[k][i] * cosf - sinf * a[k][j];
 		tempclnj[k] = a[k][i] * sinf + cosf * a[k][j];
@@ -44,13 +42,43 @@ void mulmatr(int n, double **a,double **t, double cosf, double sinf, int i, int 
 	tempstrj[i] = tempclni[j];
 	tempstrj[j] = tempclnj[j];
 
+	//пересчитываем массив сумм строк
+	if (mode == 2) {
+		sumstr[i] = 0;
+		sumstr[j] = 0;
+		for (int k = 0 ; k < n; ++k) {
+			sumstr[i] += tempstri[k] * tempstri[k] * (k != i);
+			sumstr[j] += tempstrj[k] * tempstrj[k] * (k != j) ;
+			if ( k!= i && k != j) {
+				sumstr[k] -= a[k][i] * a[k][i] ;
+				sumstr[k] -= a[k][j] * a[k][j] ;
+				sumstr[k] += tempclni[k] * tempclni[k] ;
+				sumstr[k] += tempclnj[k] * tempclnj[k] ;
+			}
+			
+		}
+
+	}
+
+	//считаем end
+	for(int k = 0 ; k < n; ++k) {
+		end -= abs(a[i][k]) * ( i != k);
+		end -= abs(a[j][k]) * ( j != k);
+		end -= abs(a[k][i]) * ( i != k ) * (k != j);
+		end -= abs(a[k][j]) * ( j != k ) * (k != i);
+		end += abs(tempstri[k]) * ( i != k);
+		end += abs(tempstrj[k]) * ( j != k);
+		end += abs(tempclni[k]) * ( i != k ) * (k != j);;
+		end += abs(tempclnj[k]) * ( j != k ) * (k != i);
+	}
+
+
 	for(int k = 0 ; k < n; ++k) {
 		a[i][k] = tempstri[k];
 		a[j][k] = tempstrj[k];
 		a[k][i] = tempclni[k];
 		a[k][j] = tempclnj[k];
 	}
-	
 }
 
 
@@ -85,28 +113,45 @@ double func(int i,int j, int n) {
 
 
 
-void selection(int mode, int &i1, int &j1, double ** a, int n) {
+void selection(int mode, int &i1, int &j1, double ** a, int n, double *sumstr) {
 	switch (mode) { 
 		case 1:
 			{
-
-			double max = 0;
-			for ( int i = 0; i < n; ++i) {
-				for (int j = 0; j < n; ++j) {
-					if (abs(a[i][j]) * (i != j) > max) {
-						i1 = i;
-						j1 = j;
-						max = abs(a[i][j]);
+				double max = 0;
+				for ( int i = 0; i < n; ++i) {
+					for (int j = 0; j < n; ++j) {
+						if (abs(a[i][j]) * (i != j) > max) {
+							i1 = i;
+							j1 = j;
+							max = abs(a[i][j]);
+						}
 					}
+				}
+				break;
+			}
+		case 2:
+			{
+				double maxsum = 0;
+				int maxind = 0;
+				for(int k = 0; k < n; ++k) {
+					if (sumstr[k] > maxsum) {
+						maxsum = sumstr[k];
+						maxind = k;
+					}
+				}	
+				i1 = maxind;
+				double max = 0;
+				for (int k = 0 ; k < n; ++k) {
+					if ( abs(a[i1][k]) * (k != i1) > max) {
+						j1 = k;
+						max = abs(a[i1][k]);
 				}
 			}
 			break;
 			}
-		case 2:
-			break;
 		case 3:
 			if ( j1 == n - 1) {
-				if (i1 == n - 1) {
+				if (i1 == n - 2) {
 					i1 = 0;
 					j1 = 1;
 					return;
@@ -131,6 +176,7 @@ void selection(int mode, int &i1, int &j1, double ** a, int n) {
 
 void answer(int n, double **a, double **t, int mode) {
 	double end = 0;
+	long long iter = 0;
 	for( int i = 0 ; i < n; ++i) {
 		for( int j = 0 ; j < n; ++j) {
 			end += abs(a[i][j]) * (i != j);
@@ -140,14 +186,21 @@ void answer(int n, double **a, double **t, int mode) {
 	for(int i = 0 ; i < n; ++i) {
 		sumstr[i] = 0;
 		for (int j = 0 ; j < n ; ++j) {
-			sumstr[i] += a[i][j] * (i != j);
+			sumstr[i] += a[i][j] * a[i][j] * (i != j);
 		}
 	}
 
+
+	int i = 0;
+	int j = 0;
 	while ( end > eps) {
-		int i = 0;
-		int j = 0;
-		selection(mode, i, j, a, n);
+		++iter;
+		selection(mode, i, j, a, n, sumstr);
+		//printmatr(n,a);
+		//for (int k = 0 ; k < n; ++k) {
+		//	cout<< sumstr[k] << endl;
+		//}
+		//cout << endl << i << j << endl;
 		double x = - 2 * a[i][j];
 		double y = a[i][i] - a[j][j];
 		double cosf, sinf;
@@ -158,18 +211,14 @@ void answer(int n, double **a, double **t, int mode) {
 			cosf = sqrt(1.0/2 + abs(y)/(2 * sqrt(x * x + y * y)));
 			sinf = sgn( x * y) * abs(x) / (2 * cosf * sqrt(x*x + y * y));
 		}
-		//cout << "cos sin" << cosf << ' ' << sinf << endl;
-		mulmatr(n, a, t, cosf, sinf,i, j);
-		/*if (abs(a[i][j]) < eps) {
-			cout << "a :" << endl;
-			printmatr(n,a);
-			cout << "t :" << endl;
-			printmatr(n,t);
-			return ;
-		}*/
-		cin >> x;
-		printmatr(n,a);
-		//cout << i << ' ' << j << endl;
+
+		mulmatr(n, a, t, cosf, sinf,i, j, end, mode, sumstr);
+		//cin >> x;
+		
+	}
+	cout << "number of iterations = " << iter << endl << "lambda:" << endl;
+	for (int k = 0 ; k < n; ++k) {
+		cout << a[k][k] << ' ';
 	}
 	
 }
@@ -179,7 +228,7 @@ void answer(int n, double **a, double **t, int mode) {
 int main(){
 	int n;
 	double **a;
-
+	clock_t time;
 	cout << "1 - fromfile, 2 - generate" << endl;
 	int genmatr;
 	cin >> genmatr;
@@ -217,7 +266,9 @@ int main(){
 				a[i][j] = func(i,j,n);
 			}
 		}
-		printmatr(n,a);
+		if ( n <= 10) {
+			printmatr(n,a);
+		}
 	}
 	double **t = new double *[n];
 	for (int i = 0; i < n; ++i) {
@@ -228,8 +279,10 @@ int main(){
 			t[i][j] = (i == j);
 		}
 	}
-	printmatr(n,t);
+	time = clock();
 	answer(n, a, t, mode);
+	time = - clock();
+	cout << endl << setprecision(6) << 1000*((double) -time)/CLOCKS_PER_SEC << "ms" <<endl;
 	freemem(a,n);
 	freemem(t,n);
 	return 0;
